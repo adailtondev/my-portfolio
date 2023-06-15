3fc831f906bb79fc878b2747ddb51923cd5e9718
adailtondev <adailtonh.dev@gmail.com>
2023-06-15 08:28:13 -0300

    der

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["typewriter-effect"],
+      external: ["typewriter-effect/dist/core"],
     },
   },
 });
